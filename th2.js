a687f4b92a28d6af872736c5d1a153cb49a19afa
thaonguyen0305 <nguyenthaotran35@gmail.com>
2023-07-22 14:31:36 +0700

    baitap2

---
+++ th2.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt('Enter the Width');
+inputheight = prompt('Enter the Height');
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write("The area is: " + area);
