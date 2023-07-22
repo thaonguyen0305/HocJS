8bdba43a485c63660c37b5afd30e9adbd8238bb4
thaonguyen0305 <nguyenthaotran35@gmail.com>
2023-07-22 15:14:06 +0700

    bt thuchanh

---
+++ th5.js
@@ -0,0 +1,7 @@
+function tan(){
+    let r = document.getElementById("radius").value;
+    let C = 2*3.14*r;
+    document.getElementById("chuvihinhtron").innerHTML=C;
+    let S = 3.14*r*r;
+    document.getElementById('dientichhinhtron').innerHTML=S;
+    } 
