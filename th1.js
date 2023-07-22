6588617a98afad7c5baf0298a01556382fbf0388
thaonguyen0305 <nguyenthaotran35@gmail.com>
2023-07-22 14:08:32 +0700

    bai tap js dau tien

---
+++ th1.js
@@ -0,0 +1,34 @@
+document.write('Bài tập 1')
+document.write('<br/>')
+let i = 10
+let f = 20.5
+let b = true
+let s = "Hà Nội"
+document.write('i= ' + i)
+document.write('<br/>')
+document.write('f= ' + f)
+document.write('<br/>')
+document.write('b= ' + b)
+document.write('<br/>')
+document.write('s= ' + s)
+
+document.write('<br/>')
+document.write('<br/>')
+document.write('Bài tập 2')
+document.write('<br/>')
+let width = 10
+let height = 20
+let z = width * height
+document.write('Area= '+ z)
+
+document.write('<br/>')
+document.write('<br/>')
+document.write('Bài tập 3')
+document.write('<br/>')    
+let A = prompt("Nhap A:")
+let B = prompt("Nhap B:")
+if(A%B == 0){
+    alert('A chia het cho B')
+} else{
+    alert('A khong chia het cho B')
+}
