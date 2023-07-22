492212c09ffa33e516125ec528e5ad6e54e84d5d
thaonguyen0305 <nguyenthaotran35@gmail.com>
2023-07-22 15:56:55 +0700

    chuyendoinhietdo

---
+++ chuyendoinhietdo.js
@@ -0,0 +1,5 @@
+function chuyendoi() {
+    let c = document.getElementById("doC").value;
+    let f = (9*c+(32*5))/5;
+    document.getElementById("ketqua").innerHTML= f;
+}
--- th5.js
+++ th5.js
@@ -1,5 +1,5 @@
 function hinhtron(){
-    let r = document.getElementById("radius").value;
+    let r = document.getElementById("bankinh").value;
     let C = 2*3.14*r;
     document.getElementById("chuvihinhtron").innerHTML=C;
     let S = 3.14*r*r;
